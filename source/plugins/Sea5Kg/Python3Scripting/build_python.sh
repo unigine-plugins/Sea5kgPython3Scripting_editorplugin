eb2e38e07890130dc9569bc3adda09abd5165003
Evgenii Sopov <mrseakg@gmail.com>
2023-06-13 17:34:35 +0700

    Fix build for linux and added project

--- source/plugins/Sea5Kg/Python3Scripting/build_python.sh
+++ source/plugins/Sea5Kg/Python3Scripting/build_python.sh
@@ -26,3 +26,6 @@ check_ret $? "Configure python"
 
 make install
 check_ret $? "Build python"
+
+mkdir -p `pwd`/../../../../../bin/Python3Home/lib
+cp -rf `pwd`/../python-bin-linux/lib/python3.10 `pwd`/../../../../../bin/Python3Home/lib/python3.10
