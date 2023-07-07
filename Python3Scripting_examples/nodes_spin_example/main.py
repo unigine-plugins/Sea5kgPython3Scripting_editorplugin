fb71f3b65c6e801c7c4797652108031c945c65ce
Evgenii Sopov <mrseakg@gmail.com>
2023-07-07 09:48:58 +0700

    Fix typos

--- Python3Scripting_examples/nodes_spin_example/main.py
+++ Python3Scripting_examples/nodes_spin_example/main.py
@@ -4,19 +4,19 @@ print('Nodes')
 
 i = 0
 _max = 30
-angels_diff = 0.2
-angels_cur = 0
+angles_diff = 0.2
+angles_cur = 0
 sleep_s = 0.026
 while i < _max:
     i += 1
     for node in LIST_NODES:
-        node.rotate(angels_cur, angels_cur, angels_cur)
-    angels_cur += angels_diff
+        node.rotate(angles_cur, angles_cur, angles_cur)
+    angles_cur += angles_diff
     time.sleep(sleep_s)
 i = 0
 while i < _max:
     i += 1
     for node in LIST_NODES:
-        node.rotate(-angels_cur, -angels_cur, -angels_cur)
-    angels_cur -= angels_diff
+        node.rotate(-angles_cur, -angles_cur, -angles_cur)
+    angles_cur -= angles_diff
     time.sleep(sleep_s)
