2443040090cf94f5c3a72b477c64ffeea25366a8
Evgenii Sopov <mrseakg@gmail.com>
2023-10-12 00:12:59 +0700

    Updated .gitignore

--- source/plugins/Sea5kg/Python3Scripting/build_python.sh
+++ source/plugins/Sea5kg/Python3Scripting/build_python.sh
@@ -15,17 +15,28 @@ check_ret() {
     fi
 }
 
+apt install libffi-dev
+
 cd Python-3.10.1
 
+echo "Configuring Python..."
+# LDFLAGS="-static" --disable-shared \
 export CFLAGS="$CFLAGS -fPIC"
+export LDFLAGS="$CFLAGS -rdynamic"
 ./configure \
    --enable-optimizations \
    --prefix=`pwd`/../python-bin-linux \
-   --with-static-libpython
-check_ret $? "Configure python"
+   --libdir=`pwd`/../python-bin-linux/lib \
+   --with-static-libpython > ../configure_python.log 2>&1
+check_ret $? "Configured python"
 
-make install
+# make -j8 LDFLAGS="-static" LINKFORSHARED=" " > ../build_python.log 2>&1
+make -j8 LINKFORSHARED=" " > ../build_python.log 2>&1
 check_ret $? "Build python"
+
+make LINKFORSHARED=" " install > ../install_python.log 2>&1
+check_ret $? "Install python"
+
 if [ -d `pwd`/../../../../../bin/plugins/Sea5kg/Python3Scripting/Python3Home ]; then
     rm -rf `pwd`/../../../../../bin/plugins/Sea5kg/Python3Scripting/Python3Home
 fi
